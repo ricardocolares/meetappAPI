a91a3584dc632c1b450ab373dd565748e1c44ca4
Ricardo Colares <ricardo.colares@osf-global.com>
2019-08-17 19:19:57 -0300

    aplicacao contectando com o banco de dados

---
+++ backend/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'meetapp',
+  password: 'meetapp',
+  database: 'meetappdb',
+  port: '6544',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
