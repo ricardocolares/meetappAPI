8f3669252a81e321dabd5df0f01c71b007f37a63
Ricardo Colares <ricardo.colares@osf-global.com>
2019-08-20 23:16:57 -0300

    organizing controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middleware/auth';
 import MeetupController from './app/controllers/MeetupController';
 import SubscriptionController from './app/controllers/SubscriptionController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,10 +25,11 @@ routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 
-// routes.get('/organizing', OrganizingController.index);
 routes.get('/subscriptions', SubscriptionController.index);
 
 routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
+
+routes.get('/organizing', OrganizingController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
